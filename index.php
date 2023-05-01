e3ae7e26c99394e70a75f55d19993fb0a42382f2
Ricardo Hernandez Limon <richi8297@gmail.com>
2023-04-30 18:24:32 -0600

    index corregido

--- index.php
+++ index.php
@@ -7,16 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba compartida</title>
-</head>
-<body>
 <h1>Iniciar Sesión</h1>
 	<form method="post" action="login.php">
 		<label for="matricula">Matrícula:</label>
