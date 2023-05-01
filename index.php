baae66ef7f90ba751a928313accfbc9a7ce4f93b
Ricardo Hernandez Limon <richi8297@gmail.com>
2023-04-30 18:18:19 -0600

    Prueba control

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Prueba compartida</title>
 </head>
 <body>
 <h1>Iniciar Sesión</h1>
